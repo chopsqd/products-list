097d7b0d7e2b4f72ffc955093c791bf16a5c81e6
Michael Chop <007micle007@gmail.com>
2022-11-04 00:19:39 +0300

    Made the basis of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Product} from "./components/Product";
+import {useProducts} from "./hooks/useProducts";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {products, error, loading} = useProducts()
+
+    return (
+        <div className="container mx-auto max-w-2xl pt-5">
+            {loading && <Loader />}
+            {error && <ErrorMessage error={error}/>}
+            {products.map(product => <Product key={product.id} product={product}/>)}
+        </div>
+    );
 }
 
 export default App;
